aa766c6802aae4d459d899b9660424a52b6b95f9
Michael-Choi <Michael.choi@queensu.ca>
2019-09-02 16:49:58 +0000

    added .ejs templates and routing for shortened urls

--- express_server.js
+++ express_server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.set("view engine", "ejs");
 const PORT = 8080; // default port 8080
 
 const urlDatabase = {
@@ -14,12 +15,28 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: req.params.longURL
+  };
+  res.render("urls_show", templateVars);
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
 app.get("/set", (req, res) => {
   const a = 1;
   res.send(`a = ${a}`);

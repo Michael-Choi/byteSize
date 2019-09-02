43784a62c5d0ab53dd3c84c7d767f0085fc227af
Michael-Choi <Michael.choi@queensu.ca>
2019-09-02 18:20:07 +0000

    added app.post functionality to see the form response being sent to /url/

--- express_server.js
+++ express_server.js
@@ -3,6 +3,9 @@ const app = express();
 app.set("view engine", "ejs");
 const PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -18,6 +21,15 @@ app.listen(PORT, () => {
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/:shortURL", (req, res) => {

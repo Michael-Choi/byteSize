6ef3059de46e6c48269711068e48185d6eb1541f
Michael-Choi <Michael.choi@queensu.ca>
2019-09-03 18:07:40 +0000

    added cookie tracking functionality/log in/out

--- express_server.js
+++ express_server.js
@@ -3,6 +3,9 @@ const app = express();
 app.set("view engine", "ejs");
 const PORT = 8080; // default port 8080
 
+var cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -15,6 +18,12 @@ app.get("/", (req, res) => {
   res.send("Hello world!");
 });
 
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username);
+  console.log(req.cookies);
+  res.redirect("/urls");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
@@ -24,7 +33,10 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = {
+    username: req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.post("/urls", (req, res) => {
@@ -46,10 +58,16 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect("/urls/");
 });
 
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls/");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = {
     shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL]
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]
   };
   res.render("urls_show", templateVars);
 });
@@ -60,7 +78,7 @@ app.post("/urls/:shortURL/edit", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
